bc9b1be61353ecc400ab4cdb6976241a952d0fa0
maxIrvine <max.irvine99@gmail.com>
2022-04-02 15:55:40 -0400

    adjusted map coordinates

--- acacia-client/src/components/map.tsx
+++ acacia-client/src/components/map.tsx
@@ -7,9 +7,9 @@ export default function LayeredMap() {
   return (
     <Map
         initialViewState={{
-          longitude: -100,
-          latitude: 40,
-          zoom: 3.5
+          longitude: -84.390165106,
+          latitude: 33.772163578,
+          zoom: 15
         }}
         style={{width: '100vw', height: '100vh'}}
         mapStyle="mapbox://styles/mapbox/streets-v9"
